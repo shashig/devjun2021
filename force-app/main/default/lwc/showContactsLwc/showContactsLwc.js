ef8460bcdf8a52bb1574c9763d9b12107d57d7e9
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2021-08-27 19:27:51 +0530

    JS Async/Await for JS filtering on data returned from Apex

--- force-app/main/default/lwc/showContactsLwc/showContactsLwc.js
+++ force-app/main/default/lwc/showContactsLwc/showContactsLwc.js
@@ -4,6 +4,8 @@ import getContactJs from '@salesforce/apex/ContactsController.getContact'
 
 export default class ShowContactsLwc extends LightningElement {
     contacts
+    searchResults
+    searchParm = 'Shashi'
     @api recordId = '0035g000008lqAnAAI'
     
     @wire(getContactsJs) wiredContacts
@@ -15,7 +17,12 @@ export default class ShowContactsLwc extends LightningElement {
         this.getContactsFromSalesforce()
     }
 
-    getContactsFromSalesforce() {
+    connectedCallback() {
+        console.log('In connectedCallback')
+        this.searchContacts();
+    }
+
+    async getContactsFromSalesforce() {
         getContactsJs()
         .then(response => {
             this.contacts = response;
@@ -23,5 +30,30 @@ export default class ShowContactsLwc extends LightningElement {
         .catch(error => {
             console.log(JSON.stringify(error));
         });
+    }
+
+
+    searchContacts() {
+        //First check if contacts are loaded. If no, then wait for the load to complete
+        if (!this.contacts) {
+            //Anonymous Async function as await can't be used in regular functions
+            //Reference: https://javascript.info/async-await
+            (async() => {
+                this.contacts = await getContactsJs(); //Call Apex to get all contacts
+                console.log(this.contacts.length)
+                this.filterContacts() //Call JS filter method
+            })();
+        } else {
+            this.filterContacts()
+        }
+    }
+
+    filterContacts() {
+        //Filter on the contacts
+        let searchResults = this.contacts.filter(contact => {
+            console.log(JSON.stringify(contact))
+            return (contact.Name.includes(this.searchParm))
+        })
+        console.log(JSON.stringify(searchResults))
     }
 }
