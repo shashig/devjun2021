8e7cb41eac1897f6537bf1281c51634b0e7ed103
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2021-09-13 18:52:33 +0530

    Resolving issue with the array display

--- force-app/main/default/lwc/arrayDisplayIssue/arrayDisplayIssue.js
+++ force-app/main/default/lwc/arrayDisplayIssue/arrayDisplayIssue.js
@@ -2,20 +2,30 @@ import { LightningElement } from 'lwc';
 
 export default class ArrayDisplayIssue extends LightningElement {
     employees;
+    empCount;
     guides = [{"name": "Abhijith"}, {"name": "Sanjay"}]
-
+    connectedCallback() {
+        console.log('Came to connectedCallback');
+    }
+    renderedCallback() {
+        console.log('Came to renderedCallback');
+    }
     showEmployees(event) {
-        console.log(JSON.stringify(event.currentTarget))
+        event.preventDefault();
+        //console.log(JSON.stringify(event.currentTarget))
         const guideName = event.currentTarget.textContent;
         console.log(guideName)
         if (guideName === "Abhijith") {
             this.employees = [{"name": "Shashi"}, {"name": "Dinesh"}, {"name": "Manasa"}];
         } else {
-            this.employees = [{"name": "Veena"}, {"name": "Aditya"}, {"name": "Sheela"}];
+            this.employees = [{"name": "Veena"}, {"name": "Aditya"}];
         }
+        this.empCount = this.employees.length
+        console.log('>>> Employee Count: ' + this.empCount);
     }
 
-    showGuides() {
+    showGuides(event) {
+        event.preventDefault();
         this.employees = undefined;
     }
 }
