3e8f083c589732a8206f1b19f0eecc1d6ecb8a5e
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2022-02-23 12:18:26 +0530

    Apex Integration Services Session - 22Feb2022

--- force-app/main/default/lwc/simpleLWC/simpleLWC.js
+++ force-app/main/default/lwc/simpleLWC/simpleLWC.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 
 export default class SimpleLWC extends LightningElement {
-    name = 'Have Fun Solving'
+    name = 'Stratos'
 
     connectedCallback() {
         console.log('came into connectedCallback')
